2a4cb9a0a40fc2b2e429533b429dcc38b5d6f0ae
Anton Tyutin <anton@tyutin.ru>
2014-03-21 09:59:28 +0400

    Fix php warnings on phpunit test failures.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,3 +1,7 @@
 <?php
 
 require __DIR__ . '/../vendor/autoload.php';
+
+// avoiding of Yii Framework class autoloader warnings
+spl_autoload_unregister(array('YiiBase','autoload'));
+spl_autoload_register(function ($classname) { @YiiBase::autoload($classname); });
