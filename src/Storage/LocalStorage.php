1bb30536680186881c7438cfaa9fee7a05535254
Anton Tyutin <anton@tyutin.ru>
2014-03-25 18:18:13 +0400

    Fix `LocalStorage` errors. Create directory when putting a file.

--- src/Storage/LocalStorage.php
+++ src/Storage/LocalStorage.php
@@ -5,6 +5,7 @@
 
 namespace Infotech\FileStorage\Storage;
 
+use Infotech\FileStorage\File\FileDescriptor;
 use Infotech\FileStorage\Storage\Exception;
 
 class LocalStorage extends AbstractStorage
@@ -16,17 +17,35 @@ class LocalStorage extends AbstractStorage
         if (null === $this->basePath) {
             throw new Exception\ConfigurationException('The "basePath" parameter must be specified.');
         }
-        if (is_dir($this->basePath)) {
+        if (!is_dir($this->basePath)) {
             throw new Exception\ConfigurationException('The specified basePath not exists or isn\'t a directory.');
         }
     }
 
+    public function put($path, FileDescriptor $file)
+    {
+        if (false === mkdir(dirname($this->createLocalPath($path)), 0777, true)) {
+            throw new Exception\OperationFailureException('Path creation failure');
+        }
+        parent::put($path, $file);
+    }
+
+
     /**
-     * @param $path
+     * @param string $path
      * @return string
      */
     protected function createUri($path)
     {
-        return 'file://' . $this->basePath . '/'. trim($path, '/');
+        return 'file://' . $this->createLocalPath($path);
+    }
+
+    /**
+     * @param string $path
+     * @return string
+     */
+    protected function createLocalPath($path)
+    {
+        return $this->basePath . '/' . trim($path, '/');
     }
 }
