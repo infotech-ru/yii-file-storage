18e5f221706e7d7bd7d687f5b1c1ac04c6c93ad8
Anton Tyutin <anton@tyutin.ru>
2014-04-02 10:35:30 +0400

    Moving library to public domain.

--- src/File/FileDescriptor.php
+++ src/File/FileDescriptor.php
@@ -1,6 +1,11 @@
 <?php
-/**
- * @author Anton Tyutin <anton@tyutin.ru>
+/*
+ * This file is part of the infotech/yii-file-storage package.
+ *
+ * (c) Infotech, Ltd
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Infotech\FileStorage\File;
